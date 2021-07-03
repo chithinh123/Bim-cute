d55a5a49bfe1d4946c93019a27dcdc35ea620844
chithinh123 <86840532+chithinh123@users.noreply.github.com>
2021-07-03 19:08:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Ngủ ngon nhé:3.',
+    messDesc: 'Ngủ ngon nhé Thanh Nhan:3.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http:// http://www.facebook.com/chi.thinh11205.com'
 }
